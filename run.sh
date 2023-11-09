58596c4f45fa23fba42611932534867d62e7b910
Matthew Feickert <matthew.feickert@cern.ch>
2023-11-09 15:33:51 -0600

    fix: Add setup of AnalysisBase environment

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # . /environment
+
+# Setup AnalysisBase environment in the event "$@" doesn't create a new shell
+. /release_setup.sh
+
 echo $PATH
 echo "========= all set up. ============"
 ls
