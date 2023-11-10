d7a0fcab9961e0eb99c7d445d900e73007bddf61
Matthew Feickert <matthew.feickert@cern.ch>
2023-11-09 18:33:17 -0600

    feat: Set jupyter shell to bash to harmonize with AnalysisBase
    * The default without this is /bin/sh which is not as perferable.

--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -3,3 +3,5 @@ c.ServerApp.port = 9999
 # c.ServerApp.root_dir = ''
 c.ServerApp.allow_password_change = False
 c.NotebookApp.open_browser = False
+# Set shell to bash as AnalysisBase assumes it
+c.NotebookApp.terminado_settings = { "shell_command": ["/bin/bash"] }
