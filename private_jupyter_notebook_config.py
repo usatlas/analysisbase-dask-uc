5e2a526a7e9c66a19f2161774807f6184f91a265
Ilija Vukotic <ivukotic@uchicago.edu>
2023-12-06 14:27:50 -0600

    correct place to change config

--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -5,4 +5,13 @@ c.ServerApp.allow_password_change = False
 c.NotebookApp.open_browser = False
 # Set shell to bash as AnalysisBase assumes it
 # force login shell to pickup ~/.bash_profile
-c.NotebookApp.terminado_settings = { "shell_command": ["/bin/bash", "-l"] }
+c.NotebookApp.terminado_settings = {"shell_command": ["/bin/bash", "-l"]}
+
+
+def host_allowlist(handler, host):
+    handler.log.info("Request to proxy to host " + host)
+    return host.startswith("192.170") or host.startswith("2605:9a00:10:200a") or host.startswith("dask-")
+
+
+# for dask dashboards to be visible
+c.ServerProxy.host_allowlist = host_allowlist
