ed0b8b4102176f1efa5c1e7a9ee19e9cda97d7eb
Ilija Vukotic <ivukotic@uchicago.edu>
2023-12-07 14:07:02 -0600

    trying full open

--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -9,8 +9,9 @@ c.NotebookApp.terminado_settings = {"shell_command": ["/bin/bash", "-l"]}
 
 
 def host_allowlist(handler, host):
-    handler.log.info("Request to proxy to host " + host)
-    return host.startswith("192.170") or host.startswith("2605:9a00:10:200a") or host.startswith("dask-")
+    # handler.log.info("Request to proxy to host " + host)
+    return True
+    # return host.startswith("192.170") or host.startswith("2605:9a00:10:200a") or host.startswith("dask-")
 
 
 # for dask dashboards to be visible
