c2629dadd5b51e430214a9ea75d6b9f844fdf898
Ilija Vukotic <ivukotic@uchicago.edu>
2023-12-11 14:56:02 -0600

    Fix daskboard proxy configuration

--- private_jupyter_notebook_config.py
+++ private_jupyter_notebook_config.py
@@ -6,3 +6,13 @@ c.NotebookApp.open_browser = False
 # Set shell to bash as AnalysisBase assumes it
 # force login shell to pickup ~/.bash_profile
 c.NotebookApp.terminado_settings = {"shell_command": ["/bin/bash", "-l"]}
+
+
+def host_allowlist(handler, host):
+    handler.log.info("Request to proxy to host " + host)
+    return True
+    # return host.startswith("192.170") or host.startswith("2605:9a00:10:200a") or host.startswith("dask-")
+
+
+# for dask dashboards to be visible
+c.ServerProxy.host_allowlist = host_allowlist
