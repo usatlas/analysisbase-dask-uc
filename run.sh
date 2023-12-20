c06974e247b2c7d15e88b753547b833e3dd5671d
Matthew Feickert <matthew.feickert@cern.ch>
2023-12-19 20:20:48 -0600

    chore: Remove /release_setup.sh from CMD (#5)
    * As /release_setup.sh is now controlled through the base image's
      ENTRYPOINT there is no need to source /release_setup.sh at CMD time,
      and so it can be removed.

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # . /environment
 
-# Setup AnalysisBase environment in the event "$@" doesn't create a new shell
-. /release_setup.sh
-
 echo $PATH
 echo "========= all set up. ============"
 ls
